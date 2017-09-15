5e52fc2a660b4aef6f4e4f60897b8c25e6c0a625
Lu Chen <76916242@qq.com>
2017-09-15 17:35:25 +0800

    Create Repo

---
+++ PSDtoNGUI/Editor/PSDGenerator.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using PhotoshopFile;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace PSDGenerator
+{
+    public static class PSDUtils
+    {
+        public static PsdFile ImportPSD(string path)
+        {
+            PsdFile psd = null;
+            psd = new PsdFile(path, Encoding.Default);
+            Debug.Log("Import PSD File " + Path.GetFileNameWithoutExtension(path));
+            return psd;
+        }
+
+        public static void Generator(PsdFile psd, UIAtlas uiAtlas, string rootName)
+        {
+            Debug.Log("Generate Start!");
+            Debug.Log(psd.BaseLayer.Rect.width + "  " + psd.BaseLayer.Rect.height);
+            GameObject root = null;
+            if (rootName == null || (root = GameObject.Find(rootName)) == null)
+            {
+                Debug.LogError("Root not found!");
+                return;
+            }
+            foreach (Layer layer in psd.Layers)
+            {
+                if (layer.Name != "</Layer set>" &&
+                    layer.Name != "</Layer group>" &&
+                    layer.Visible &&
+                    layer.Rect.width > 0 &&
+                    layer.Rect.height > 0)
+                {
+                    Debug.Log(layer.Name);
+                    Debug.Log(layer.Rect.width + "  " + layer.Rect.height);
+                    string pattern = "label_";
+                    Regex regexInstance = new Regex(pattern);
+                    if (regexInstance.IsMatch(layer.Name))
+                    {
+                        UILabel label = NGUITools.AddWidget<UILabel>(root, 1);
+                        label.pivot = UIWidget.Pivot.TopLeft;
+                        Vector3 spritePos = new Vector3(layer.Rect.position.x - psd.BaseLayer.Rect.width / 2, psd.BaseLayer.Rect.height / 2 - layer.Rect.position.y);
+                        Debug.Log("before: " + layer.Rect.position + " after: " + spritePos);
+                        label.name = layer.Name;
+                        label.width = (int)layer.Rect.width;
+                        label.height = (int)layer.Rect.height;
+                        label.transform.localPosition = spritePos;
+                        label.pivot = UIWidget.Pivot.Center;
+                    }
+                    else
+                    {
+                        UISprite sprite = NGUITools.AddSprite(root, uiAtlas, layer.Name, 1);
+                        sprite.pivot = UIWidget.Pivot.TopLeft;
+                        Vector3 spritePos = new Vector3(layer.Rect.position.x - psd.BaseLayer.Rect.width / 2, psd.BaseLayer.Rect.height / 2 - layer.Rect.position.y);
+                        Debug.Log("before: " + layer.Rect.position + " after: " + spritePos);
+                        sprite.name = layer.Name;
+                        sprite.width = (int)layer.Rect.width;
+                        sprite.height = (int)layer.Rect.height;
+                        sprite.transform.localPosition = spritePos;
+                        sprite.pivot = UIWidget.Pivot.Center;
+                        sprite.type = UISprite.Type.Sliced;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PSDtoNGUI/Editor/PSDtoNGUIEditor.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using PSDGenerator;
+using PhotoshopFile;
+
+public class PSDGeneratorWindow:EditorWindow
+{
+    static string rootName;
+    static Object psdObj;
+    static UIAtlas uiAtlas;
+    static string psdPath;
+
+    public static PSDGeneratorWindow window;
+
+    [MenuItem("DivaBuild/Client Tools/PSD Generator", false, 652)]
+    public static void OpenPSDGeneratorWindow()
+    {
+        window = EditorWindow.GetWindow<PSDGeneratorWindow>();
+        
+    }
+
+    void OnGUI()
+    {
+        EditorGUILayout.BeginVertical();
+        rootName = EditorGUILayout.TextField("Root Name:",rootName);
+        psdObj = EditorGUILayout.ObjectField("Choose Psd File:", psdObj,typeof(Object), false);
+        uiAtlas = (UIAtlas)EditorGUILayout.ObjectField("Choose Atlas File:", uiAtlas, typeof(UIAtlas), false);
+        psdPath = AssetDatabase.GetAssetPath(psdObj);
+        EditorGUILayout.EndVertical();
+        if (GUILayout.Button("Generate"))
+        {
+            PsdFile psd = PSDUtils.ImportPSD(psdPath);
+            if (psd != null)
+                PSDUtils.Generator(psd, uiAtlas, rootName);
+            else
+                Debug.LogError("Can not read the PSD Atlass File: " + psdPath);
+        }
+    }
+}
